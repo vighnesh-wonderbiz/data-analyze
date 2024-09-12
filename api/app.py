784f52b8772dfc9501d60702d9cc8c23b05c81cf
Vighnesh Manjrekar <vighnesh.manjrekar@wonderbiz.in>
2024-09-12 17:30:02 +0530

    Dev: v2.0 12/9

--- api/app.py
+++ api/app.py
@@ -3,15 +3,20 @@ from typing import Optional
 from datetime import datetime
 from utils import Utils
 from urllib.parse import unquote
+import pandas as pd
 
 app = FastAPI()
 
 @app.get("/bar-chart")
 async def BarChart(date: Optional[str] = None):
-    date = unquote(date)
+    if date:
+        if date.startswith('"'):
+            date = date.split('"')[1]
+        date = unquote(date)
     if not date:
         print(date)
         date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    print(date)
     l = Utils(date)
     res = l.generate_bar_data()
     return res
