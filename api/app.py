289b15a07212e908d86c230369b3aff975e704a8
Rohit Wahwal <rohit.wahwal@wonderbiz.in>
2024-09-12 12:22:17 +0530

    added barchart api

---
+++ api/app.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from typing import Optional
+from datetime import datetime
+from ..utils import Utils
+
+app = FastAPI()
+
+@app.get("/barChart")
+async def BarChart(date: Optional[str] = None):
+    if not date:
+        date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    
+    l = Utils(date)
+    res = l.generate_weekly_data()
+    return res
